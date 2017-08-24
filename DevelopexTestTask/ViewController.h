3f7e3d62fb311814e6f5473cce453e32a43029c6
Boriszinkovich <boriszinkovich@gmail.com>
2017-08-24 16:34:06 +0300

    Initial Commit

---
+++ DevelopexTestTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DevelopexTestTask
+//
+//  Created by User on 24.08.17.
+//  Copyright © 2017 BorisZinkovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
